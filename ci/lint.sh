09c741562f0d4a75edfa6c088906e302ad774201
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-11-25 13:17:50 +0100

    try THIS

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+ERR=0
+
 echo "::add-matcher::ci/matchers/svelte.json"
-bunx sv check --output machine-verbose
+bunx sv check --output machine-verbose || ERR=1
 echo "::remove-matcher owner=svelte::"
 
 echo "::add-matcher::ci/matchers/eslint.json"
-bunx eslint --format compact
+bunx eslint --format compact || ERR=1
 echo "::remove-matcher owner=eslint::"
+
+exit $ERR
