2b28bbf7c44c441f6f915e03104ce52a465e14aa
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-12-16 16:46:52 +0100

    return typed place profile

--- src/routes/game/[gameId]/+page.server.ts
+++ src/routes/game/[gameId]/+page.server.ts
@@ -1,5 +1,6 @@
 import { error } from "@sveltejs/kit";
 import { db } from "$lib/database";
+import { PlaceProfile } from "$lib/schemas";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
@@ -13,7 +14,17 @@ export const load: PageServerLoad = async ({ params }) => {
 		error(404, { message: "No such game found." });
 	}
 
+	const parsedPlaceProfileParseResult = PlaceProfile.safeParse(game.place_profile);
+	if (!parsedPlaceProfileParseResult.success || !parsedPlaceProfileParseResult.data) {
+		error(500, { message: "Inconsistent place profile data in database." });
+	}
+
+	const parsedPlaceProfile = parsedPlaceProfileParseResult.data;
+
 	return {
-		game,
+		game: {
+			...game,
+			place_profile: parsedPlaceProfile,
+		},
 	};
 };
